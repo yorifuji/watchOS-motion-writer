dab1ed7ed4ab574dd7a6d0029eb494aeadfbc6da
yorifuji <yorifuji@gmail.com>
2020-10-06 23:52:49 +0900

    Initial Commit

---
+++ watchOS-motion-writer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  watchOS-motion-writer
+//
+//  Created by yorifuji on 2020/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
